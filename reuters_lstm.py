7762be28b4cfe0f92f365c91cc85e12648d3985f
unknown <foamliu@yeah.net>
2018-10-21 14:04:46 +0800

    update

--- reuters_lstm.py
+++ reuters_lstm.py
@@ -49,10 +49,11 @@ print('Building model...')
 model = Sequential()
 model.add(Embedding(max_words, 128))
 model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
 
 # try using different optimizers and different optimizer configs
-model.compile(loss='binary_crossentropy',
+model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
